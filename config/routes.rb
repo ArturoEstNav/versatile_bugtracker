682a67bbc0af6101102eda262b6a471c5d3ed8ce
Arturo Estrada <arturoestradanav@gmail.com>
2020-12-17 17:08:02 -0600

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # resources :users
-
+  resources :users
+  resources :projects, :memos, only: [ :create, :update, :show ]
+  resources :tickets, only: [ :create, :update, :show, :index ]
 end
