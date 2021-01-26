441ca743467c0297e34ce129972a28f77513d646
Arturo Estrada <arturoestradanav@gmail.com>
2021-01-25 23:57:08 -0600

    removed old events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_015338) do
+ActiveRecord::Schema.define(version: 2021_01_26_055409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "events", force: :cascade do |t|
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_events_on_user_id"
-  end
 
   create_table "memos", force: :cascade do |t|
     t.bigint "ticket_id", null: false
