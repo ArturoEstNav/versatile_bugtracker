32655250d11f25b6c225963400192dce9ef1fdfc
Arturo Estrada <arturoestradanav@gmail.com>
2021-01-28 12:13:33 -0600

    added new user event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_043220) do
+ActiveRecord::Schema.define(version: 2021_01_28_175245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,14 @@ ActiveRecord::Schema.define(version: 2021_01_27_043220) do
     t.bigint "ticket_id", null: false
   end
 
+  create_table "user_events", force: :cascade do |t|
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_user_events_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -88,4 +96,5 @@ ActiveRecord::Schema.define(version: 2021_01_27_043220) do
 
   add_foreign_key "memos", "tickets"
   add_foreign_key "tickets", "projects"
+  add_foreign_key "user_events", "users"
 end
