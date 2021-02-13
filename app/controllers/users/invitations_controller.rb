46bd464c7afb81e4acdda7189498d1d06e9ca882
Arturo Estrada <arturoestradanav@gmail.com>
2021-02-12 21:57:50 -0600

    invitable works as intended

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,7 +1,19 @@
 class Users::InvitationsController < Devise::InvitationsController
-   skip_before_action :require_no_authentication, only: [:new]
+  skip_before_action :require_no_authentication, only: [:new]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def new
     super
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:invite,
+                                      keys: [
+                                      :first_name,
+                                      :last_name,
+                                      :employee_number
+                                      ])
   end
 end
