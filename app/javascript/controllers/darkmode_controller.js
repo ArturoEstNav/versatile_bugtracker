415d270fcef68e51be6eaed34a5771d7a31c9786
Arturo Estrada <arturoestradanav@gmail.com>
2021-02-24 23:44:49 -0600

    added nightmode functionality

---
+++ app/javascript/controllers/darkmode_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "page", "toggle" ]
+
+  connect() {
+    if (localStorage.darkMode == 'true') {
+      this.element.classList.add("dark-mode");
+      this.toggleTarget.innerHTML = '<i class="fas fa-sun"></i>';
+    }
+  }
+
+  adjustBackground(){
+    if(this.element.classList.contains("dark-mode") ) {
+      this.element.classList.remove("dark-mode");
+      localStorage.setItem('darkMode', 'false');
+      this.toggleTarget.innerHTML = '<i class="far fa-moon"></i>';
+
+    } else {
+      this.element.classList.add("dark-mode");
+      localStorage.setItem('darkMode', 'true');
+      this.toggleTarget.innerHTML = '<i class="fas fa-sun"></i>';
+    }
+  }
+}
