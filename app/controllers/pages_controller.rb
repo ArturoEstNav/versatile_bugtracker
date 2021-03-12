bc506724a41992c176626b6c1ea0eadae19072ba
Arturo Estrada <arturoestradanav@gmail.com>
2021-03-12 11:36:45 -0600

    upgraded fonts and spacing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
-
   def home
     @projects = Project.all
     @tickets = Ticket.all
+    @user_tickets = Ticket.where(user_id: current_user)
   end
 end
