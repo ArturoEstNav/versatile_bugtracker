b6faa220a4735ba93ce6d12cf82aa4432d46aba8
Arturo Estrada <arturoestradanav@gmail.com>
2021-03-15 14:30:28 -0600

    fixed bug on navbar js controller

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,9 +1,27 @@
-  import { Controller } from "stimulus"
+import { Controller } from "stimulus"
 
-  export default class extends Controller {
-    static targets = [ "tickets", "history", "ticketsToggle", "historyToggle" ]
+export default class extends Controller {
+  static targets = [ "tickets", "history", "ticketsToggle", "historyToggle" ]
 
-    indicator() {
-      console.log(this.navbarTarget)
-    }
-  }
+  setDisplay() {
+    // unset previous display
+    this.ticketsToggleTarget.classList.remove("navbar-selected-item");
+    this.historyToggleTarget.classList.remove("navbar-selected-item");
+    this.ticketsToggleTarget.classList.add("navbar-show-item");
+    this.historyToggleTarget.classList.add("navbar-show-item");
+    this.ticketsTarget.style.display = 'none';
+    this.historyTarget.style.display = 'none';
+    // sort and set new display
+    let section = event.explicitOriginalTarget.dataset.navbarTarget
+      if(section == "ticketsToggle") {
+        console.log('tickets')
+        this.ticketsToggleTarget.classList.remove("navbar-show-item");
+        this.ticketsToggleTarget.classList.add("navbar-selected-item");
+        this.ticketsTarget.style.display = 'block';
+      } else {
+        this.historyToggleTarget.classList.remove("navbar-show-item");
+        this.historyToggleTarget.classList.add("navbar-selected-item");
+        this.historyTarget.style.display = 'block';
+      }
+   }
+}
