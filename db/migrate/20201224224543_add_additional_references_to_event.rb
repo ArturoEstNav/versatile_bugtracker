3c2c9c626c50dabe5247f2e0b84d96616e10f89e
Arturo Estrada <arturoestradanav@gmail.com>
2021-03-23 14:18:50 -0600

    removed AddAdditionalReferencesToEvent migrations from migrations folder

--- db/migrate/20201224224543_add_additional_references_to_event.rb
+++
@@ -1,6 +0,0 @@
-class AddAdditionalReferencesToEvent < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :events, :memo, foreign_key: { to_table: :memos }
-    add_reference :events, :project, foreign_key: { to_table: :projects }
-  end
-end
