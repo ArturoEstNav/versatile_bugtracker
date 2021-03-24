feb3a8c14040aa32056c5a8fc0c7a7aa92f17376
Arturo Estrada <arturoestradanav@gmail.com>
2021-03-24 11:11:23 -0600

    removed user_event related migrations from migration AddLinkToUserEvents

--- db/migrate/20210128194827_add_link_to_user_events.rb
+++ db/migrate/20210128194827_add_link_to_user_events.rb
@@ -1,6 +1,5 @@
 class AddLinkToUserEvents < ActiveRecord::Migration[6.0]
   def change
-    add_column :user_events, :event_link, :string, default: ""
     rename_column :events, :eventable_link, :event_link
   end
 end
