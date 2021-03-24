0e70bd931f139f691d66b7cd06ffdd46a2f529e5
Arturo Estrada <arturoestradanav@gmail.com>
2021-03-24 14:23:58 -0600

    removed eventable_link related migrations from migration ResetEventsTable

--- db/migrate/20210129054303_reset_events_table.rb
+++ db/migrate/20210129054303_reset_events_table.rb
@@ -2,7 +2,6 @@ class ResetEventsTable < ActiveRecord::Migration[6.0]
   def change
     remove_reference :events, :eventable, polymorphic: true
     remove_column :events, :description
-    remove_column :events, :eventable_link
 
     add_reference :events, :eventable, polymorphic: true, index: true
     add_reference :events, :user
