1be59b89fcb34b26908431a57e4a12020f1663f9
Arturo Estrada <arturoestradanav@gmail.com>
2021-03-24 13:35:30 -0600

    removed user_event dropping related migrations from migration CreateUserEvents

--- db/migrate/20210128221700_create_user_events.rb
+++ db/migrate/20210128221700_create_user_events.rb
@@ -1,6 +1,5 @@
 class CreateUserEvents < ActiveRecord::Migration[6.0]
   def change
-    drop_table :user_events
     create_table :user_events do |t|
       t.text :description
       t.references :user, null: false, foreign_key: true
