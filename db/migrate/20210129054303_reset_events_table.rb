1c07ff3aeb2b6a87755e6f24832ba1a31ed0f336
Arturo Estrada <arturoestradanav@gmail.com>
2021-03-24 14:18:59 -0600

    removed user_id related migrations from migration ResetEventsTable

--- db/migrate/20210129054303_reset_events_table.rb
+++ db/migrate/20210129054303_reset_events_table.rb
@@ -1,7 +1,6 @@
 class ResetEventsTable < ActiveRecord::Migration[6.0]
   def change
     remove_reference :events, :eventable, polymorphic: true
-    remove_reference :events, :user, index: true
     remove_column :events, :description
     remove_column :events, :eventable_link
 
